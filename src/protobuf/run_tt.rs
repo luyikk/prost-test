0975b1b1f4a92d44aa8f4149ace6d31fa6d2372f
luyikk <luyikk@126.com>
2022-04-22 23:54:22 +0800

    aa

--- src/protobuf/run_tt.rs
+++ src/protobuf/run_tt.rs
@@ -163,9 +163,9 @@ pub enum PhoneType {
 #[allow(dead_code)]
 pub const fn msg_ids()->&'static [i32]{
     &[
-        150004, //.RunTT.Foo.Fail
-        150002, //.RunTT.Fail
-        150001, //.RunTT.Foo
         150003, //.RunTT.Error
+        150002, //.RunTT.Fail
+        150004, //.RunTT.Foo.Fail
+        150001, //.RunTT.Foo
     ]
 }
